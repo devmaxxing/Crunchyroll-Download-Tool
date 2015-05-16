629dfac181d7c02cdf7cc67b463ad2fa659a32a0
oliverfei <ofei006@uottawa.ca>
2015-05-16 16:17:39 -0400

    Added project files

---
+++ Crunchyroll Download Tool/Crunchyroll Download Tool/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Crunchyroll_Download_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameBox = new System.Windows.Forms.TextBox();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.downloadList = new System.Windows.Forms.ListBox();
+            this.urlBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.addButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.downloadButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Location = new System.Drawing.Point(77, 12);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.Size = new System.Drawing.Size(100, 20);
+            this.usernameBox.TabIndex = 0;
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(77, 38);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // downloadList
+            // 
+            this.downloadList.FormattingEnabled = true;
+            this.downloadList.Location = new System.Drawing.Point(19, 90);
+            this.downloadList.Name = "downloadList";
+            this.downloadList.Size = new System.Drawing.Size(592, 147);
+            this.downloadList.TabIndex = 4;
+            // 
+            // urlBox
+            // 
+            this.urlBox.Location = new System.Drawing.Point(292, 11);
+            this.urlBox.Name = "urlBox";
+            this.urlBox.Size = new System.Drawing.Size(254, 20);
+            this.urlBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(235, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Video link:";
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(552, 10);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(59, 23);
+            this.addButton.TabIndex = 7;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addURL);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Downloads";
+            // 
+            // downloadButton
+            // 
+            this.downloadButton.Location = new System.Drawing.Point(292, 44);
+            this.downloadButton.Name = "downloadButton";
+            this.downloadButton.Size = new System.Drawing.Size(319, 23);
+            this.downloadButton.TabIndex = 9;
+            this.downloadButton.Text = "Start Download";
+            this.downloadButton.UseVisualStyleBackColor = true;
+            this.downloadButton.Click += new System.EventHandler(this.startDownload);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 248);
+            this.Controls.Add(this.downloadButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.urlBox);
+            this.Controls.Add(this.downloadList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.usernameBox);
+            this.Name = "Form1";
+            this.Text = "Crunchyroll Download Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox usernameBox;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox downloadList;
+        private System.Windows.Forms.TextBox urlBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button downloadButton;
+    }
+}
+
---
+++ Crunchyroll Download Tool/Crunchyroll Download Tool/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Crunchyroll_Download_Tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            downloadList.Items.Clear();
+        }
+
+        private void addURL(object sender, EventArgs e)
+        {
+            downloadList.Items.Add(urlBox.Text);
+        }
+
+        private void startDownload(object sender, EventArgs e)
+        {
+            String[] items = new String[downloadList.Items.Count];
+            downloadList.Items.CopyTo(items,0);
+            foreach (String item in items)
+            {
+                download(item);
+            }
+            
+        }
+
+        private void download(String url)
+        {
+            String arg1 = "no";
+            if (usernameBox.Text != null && passwordBox.Text != null)
+                arg1 = usernameBox.Text;
+            ExecuteCommandSync("start.bat " + arg1 + " " + passwordBox.Text + " " + url);
+            downloadList.Items.Remove(url);         
+        }
+
+        void ExecuteCommandSync(object command)
+        {
+            try
+            {
+                // create the ProcessStartInfo using "cmd" as the program to be run,
+                // and "/c " as the parameters.
+                // Incidentally, /c tells cmd that we want it to execute the command that follows,
+                // and then exit.
+                System.Diagnostics.ProcessStartInfo procStartInfo =
+                new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+
+                // The following commands are needed to redirect the standard output.
+                // This means that it will be redirected to the Process.StandardOutput StreamReader.
+                procStartInfo.RedirectStandardOutput = false;
+                procStartInfo.UseShellExecute = false;
+                // Do not create the black window.
+                procStartInfo.CreateNoWindow = false;
+                // Now we create a process, assign its ProcessStartInfo and start it
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+
+                proc.Start();
+                proc.WaitForExit();
+
+            }
+            catch (Exception objException)
+            {
+                Console.WriteLine(objException);
+            }
+        }
+    }
+}
